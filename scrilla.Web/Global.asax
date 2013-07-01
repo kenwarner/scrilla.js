13c3a295f2ee6f6674c78fd6bbf0fb6d2e5da2cf
Ken Warner <kjwarner@gmail.com>
2013-06-30 22:14:59 -0400

    remove original scrilla.Web

--- scrilla.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="scrilla.Web.MvcApplication" Language="C#" %>
--- scrilla.Web/scripts/scripts.js
+++
@@ -1,4 +0,0 @@
-/* An error occurred while compiling the CoffeeScript file.  Details:
-
-SyntaxError: Reserved word "function" on line 1
-*/
