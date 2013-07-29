6f5ca021a2fcf4d80342410c46fcca9d47502742
Ken Warner <kjwarner@gmail.com>
2013-07-28 21:47:44 -0400

    scrilla.Services.Tests

---
+++ scrilla.Services.Tests/AccountServiceFixture.cs
@@ -0,0 +1,54 @@
+﻿using DapperExtensions;
+using DapperExtensions.Sql;
+using scrilla.Data.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace scrilla.Services.Tests
+{
+    public class AccountServiceFixture
+	{
+		#region Setup
+
+		protected SqlConnection _sqlConnection;
+
+		public AccountServiceFixture()
+		{
+			var connectionString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+			_sqlConnection = new SqlConnection(connectionString);
+		}
+
+		~AccountServiceFixture()
+		{
+			if (_sqlConnection != null)
+				_sqlConnection.Dispose();
+		}
+
+		#endregion
+
+		#region GetAllTransactionsTests
+		[Fact]
+		public void GetAccount()
+		{
+			//var target = new AccountService(new Db(_sqlConnection));
+			//var result = target.GetAllTransactions(new DateTime(2012, 1, 1));
+			//Assert.True(result.Result.Count() == 628);
+
+			//result = target.GetAllTransactions();
+			//Assert.True(result.Result.Count() == 5208);
+
+			//result = target.GetAllTransactions(SqlDateTime.MinValue.Value, new DateTime(2012, 1, 1));
+			//Assert.True(result.Result.Count() == 4580);
+		}
+
+		#endregion
+
+	}
+}
