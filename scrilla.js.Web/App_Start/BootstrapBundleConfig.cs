dc149d46961519699822c3537f556839f87be79a
Ken Warner <kjwarner@gmail.com>
2013-09-06 19:21:15 -0400

    added CDN and fallback support for jquery and bootstap js

--- scrilla.js.Web/App_Start/BootstrapBundleConfig.cs
+++ scrilla.js.Web/App_Start/BootstrapBundleConfig.cs
@@ -12,7 +12,8 @@ namespace scrilla.js.Web.App_Start
 			// For Bootstrap theme add @Styles.Render("~/Content/bootstrap/theme") in the <head/> of your _Layout.cshtml view
 			// Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle(Links.Bundles.Scripts.bootstrap).Include(Links.scripts.bootstrap_js));
+			BundleTable.Bundles.Add(new ScriptBundle(Links.Bundles.Scripts.bootstrap, "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js") { CdnFallbackExpression = "$.fn.button" }.Include(Links.scripts.bootstrap_js));
+			//BundleTable.Bundles.Add(new StyleBundle(Links.Bundles.Styles.bootstrap, "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css") { CdnFallbackExpression = "function(url){var length=document.styleSheets.length;for(var i = 0; i < length; i++){var sheet = document.styleSheets[i];if(sheet.href=url){var rules= sheet.rules ? sheet.rules : sheet.cssRules;if(rules==null || rules.length == 0){return false;}}}}('http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css')" }.Include(Links.Content.bootstrap.bootstrap_css));
 			BundleTable.Bundles.Add(new StyleBundle(Links.Bundles.Styles.bootstrap).Include(Links.Content.bootstrap.bootstrap_css));
 			BundleTable.Bundles.Add(new StyleBundle(Links.Bundles.Styles.bootstrapTheme).Include(Links.Content.bootstrap.bootstrap_theme_css));
 		}
--- scrilla.js.Web/App_Start/BundleConfig.cs
+++ scrilla.js.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace scrilla.js.Web
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle(Links.Bundles.Scripts.jquery).Include(Links.scripts.jquery_2_0_3_js));
+			bundles.UseCdn = true;
+			bundles.Add(new ScriptBundle(Links.Bundles.Scripts.jquery, "//code.jquery.com/jquery-2.0.3.js") { CdnFallbackExpression = "window.jQuery" }.Include(Links.scripts.jquery_2_0_3_js));
 			bundles.Add(new ScriptBundle(Links.Bundles.Scripts.chosen).Include(Links.scripts.chosen_jquery_js));
 
 			bundles.Add(new StyleBundle(Links.Bundles.Styles.chosen).Include(Links.Content.chosen_css));
