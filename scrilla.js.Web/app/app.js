4761c61fd0aeb8cdaf65b46da15d05c389e2c432
Ken Warner <kjwarner@gmail.com>
2013-12-02 00:52:04 -0500

    remove ngRoute since it's not doing what I wanted

--- scrilla.js.Web/app/app.js
+++ scrilla.js.Web/app/app.js
@@ -1,24 +1,5 @@
 ﻿var scrilla = scrilla || {};
-scrilla.controllers = angular.module('scrilla.controllers', ['ngRoute']);
-scrilla.services = angular.module('scrilla.services', ['ngResource']);
 scrilla.directives = angular.module('scrilla.directives', []);
-scrilla.app = angular.module('scrillaApp', ['scrilla.controllers', 'scrilla.services', 'scrilla.directives']);
-
-scrilla.app.config(['$locationProvider', 
-	function($locationProvider) {
-		$locationProvider.html5Mode(true);
-	}]);
-
-scrilla.app.config(['$routeProvider', 
-	function ($routeProvider) {
-		$routeProvider
-			.when('/', {
-				controller: 'AccountController'
-			})
-			.when('/transactions', {
-				controller: 'TransactionController'
-			})
-			.when('/vendors', {
-				controller: 'VendorController'
-			});
-	}]);
+scrilla.controllers = angular.module('scrilla.controllers', []);
+scrilla.services = angular.module('scrilla.services', ['ngResource']);
+scrilla.app = angular.module('scrillaApp', ['scrilla.directives', 'scrilla.controllers', 'scrilla.services']);
