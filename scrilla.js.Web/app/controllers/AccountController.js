72217774017726f4b8bad9ce355700444a86b8d0
Ken Warner <kjwarner@gmail.com>
2013-12-24 16:58:55 -0500

    set page title on model load, use same scope property to bind title and h3 summary

--- scrilla.js.Web/app/controllers/AccountController.js
+++ scrilla.js.Web/app/controllers/AccountController.js
@@ -1,7 +1,12 @@
-﻿scrilla.controllers.controller('AccountController', ['$scope', '$location', 'AccountService', function ($scope, $location, AccountService) {
+﻿scrilla.controllers.controller('AccountController', ['$scope', '$location', '$document', '$filter', 'AccountService', function ($scope, $location, $document, $filter, AccountService) {
 	$scope.isNaN = isNaN;
 	$scope.from = $location.search().from;
 	$scope.to = $location.search().to;
 
 	$scope.model = AccountService.balances({ from: $scope.from, to: $scope.to });
+	$scope.model.$promise.then(function (result) {
+		$scope.model = result;
+		$scope.summary = "Accounts from " + $scope.model.dateRange.rangeSummary;
+		$document.prop('title', $scope.summary);
+	});
 }]);
