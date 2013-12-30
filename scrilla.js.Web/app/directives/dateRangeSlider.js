3944a21f3100dcd49ac004f48c9611ff37594da3
Ken Warner <kjwarner@gmail.com>
2013-12-30 00:19:53 -0500

    add month scales to the dateRangeSlider

--- scrilla.js.Web/app/directives/dateRangeSlider.js
+++ scrilla.js.Web/app/directives/dateRangeSlider.js
@@ -14,20 +14,33 @@
 					min: scope.DateRangeService.getFrom(),
 					max: scope.DateRangeService.getTo()
 				},
+				valueLabels: "hide",
 				formatter: function (val) {
 					return $filter('date')(val, 'MMMM yyyy');
 				},
 				step: {
 					months: 1
-				}
+				},
+				scales: [{
+					next: function (value) {
+						var next = new Date(value);
+						var r = new Date(next.setMonth(value.getMonth() + 1));
+						return r;
+					},
+					label: function (value, nextValue) {
+						if (value.getMonth() == 0)
+							return '\'' + $filter('date')(value, 'yy');
+
+						return $filter('date')(value, 'MMM');
+					}
+				}]
 			});
 
 			angular.element(element).bind("valuesChanged", function (e, data) {
 				$log.info('dateRangeSlider valuesChanges: ' + data.values.min + ',' + data.values.max);
 
 				scope.$apply(function () {
-					scope.DateRangeService.setFrom(data.values.min);
-					scope.DateRangeService.setTo(data.values.max);
+					scope.DateRangeService.set({ from: data.values.min, to: data.values.max });
 				});
 			});
 		}
