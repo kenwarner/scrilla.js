9164e1b5c0564ead4021d5ea70da6458aa35c238
Ken Warner <kjwarner@gmail.com>
2013-12-29 20:13:12 -0500

    add directive for jQRangeSlider

---
+++ scrilla.js.Web/app/directives/dateRangeSlider.js
@@ -0,0 +1,35 @@
+﻿scrilla.directives.directive('dateRangeSlider', ['$filter', '$log', 'DateRangeService', function ($filter, $log, DateRangeService) {
+	return {
+		restrict: 'E',
+		link: function (scope, element, attrs) {
+			scope.DateRangeService = DateRangeService;
+
+			angular.element(element).dateRangeSlider({
+				arrows: false,
+				bounds: {
+					min: scope.DateRangeService.getMin(),
+					max: scope.DateRangeService.getMax()
+				},
+				defaultValues: {
+					min: scope.DateRangeService.getFrom(),
+					max: scope.DateRangeService.getTo()
+				},
+				formatter: function (val) {
+					return $filter('date')(val, 'MMMM yyyy');
+				},
+				step: {
+					months: 1
+				}
+			});
+
+			angular.element(element).bind("valuesChanged", function (e, data) {
+				$log.info('dateRangeSlider valuesChanges: ' + data.values.min + ',' + data.values.max);
+
+				scope.$apply(function () {
+					scope.DateRangeService.setFrom(data.values.min);
+					scope.DateRangeService.setTo(data.values.max);
+				});
+			});
+		}
+	};
+}]);
