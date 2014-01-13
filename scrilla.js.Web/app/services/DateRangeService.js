1cecad9dcf92d076e70740961faef7db679579e2
Ken Warner <kjwarner@gmail.com>
2014-01-13 00:11:01 -0500

    dirty check $location.search() in setters, add isLastDayOfTheMonth() and isInSameMonth()

--- scrilla.js.Web/app/services/DateRangeService.js
+++ scrilla.js.Web/app/services/DateRangeService.js
@@ -1,7 +1,7 @@
 ﻿scrilla.services.factory('DateRangeService', ['$location', '$filter', '$document', '$log', function ($location, $filter, $document, $log) {
 	var from, to;
 	var now = new Date();
-	var min = new Date(2012, 5, 1);
+	var min = new Date(2012, 2, 1);
 	var max = new Date(now.getFullYear(), now.getMonth() + 5, 1, 0, 0, 1);
 
 	var defaultFrom = new Date(now.getFullYear(), now.getMonth() - 4, 1);
@@ -19,16 +19,17 @@
 			var locationFrom = $location.search().from;
 			var locationTo = $location.search().to;
 
+			// TODO all the $location syncing should probably be offloaded to another service
 			this.setFrom(locationFrom ? UrlToDate(locationFrom) : defaultFrom);
 			this.setTo(locationTo ? UrlToDate(locationTo) : defaultTo);
 
 			if ($location.search().from != this.toUrlFormat(from)) {
-				$log.info('resetting ?from = ' + this.toUrlFormat(from));
+				$log.info('resetting ?from = ' + $location.search().from + " -> " + this.toUrlFormat(from));
 				$location.search('from', this.toUrlFormat(from));
 			}
 
 			if ($location.search().to != this.toUrlFormat(to)) {
-				$log.info('resetting ?to = ' + this.toUrlFormat(to));
+				$log.info('resetting ?to = ' + $location.search().to + " -> " + this.toUrlFormat(to));
 				$location.search('to', this.toUrlFormat(to));
 			}
 
@@ -36,22 +37,26 @@
 		},
 
 		setFrom: function (val) {
-			if (!from || from.valueOf() != val.valueOf()) {
+			if (!from || from.getTime() != val.getTime()) {
 				$log.info('setting from = ' + val);
 				from = val;
 
-				$log.info('setting ?from = ' + this.toUrlFormat(from));
-				$location.search('from', this.toUrlFormat(from));
+				if ($location.search().from != this.toUrlFormat(from)) {
+					$log.info('setting ?from = ' + $location.search().from + " -> " + this.toUrlFormat(from));
+					$location.search('from', this.toUrlFormat(from));
+				}
 			}
 		},
 
 		setTo: function (val) {
-			if (!to || to.valueOf() != val.valueOf()) {
+			if (!to || to.getTime() != val.getTime()) {
 				$log.info('setting to = ' + val);
 				to = val;
 
-				$log.info('setting ?to = ' + this.toUrlFormat(to));
-				$location.search('to', this.toUrlFormat(to));
+				if ($location.search().to != this.toUrlFormat(to)) {
+					$log.info('setting ?to = ' + $location.search().to + " -> " + this.toUrlFormat(to));
+					$location.search('to', this.toUrlFormat(to));
+				}
 			}
 		},
 
@@ -83,10 +88,6 @@
 			return max;
 		},
 
-		summary: function() {
-			return $filter('date')(from, 'MMMM yyyy') + ' to ' + $filter('date')(to, 'MMMM yyyy');
-		},
-
 		toUrlFormat: function (date) {
 			return $filter('date')(date, 'yyyy-MM-dd');
 		},
@@ -94,6 +95,17 @@
 		lastDayOfMonth: function (strDate) {
 			var date = UrlToDate(strDate);
 			return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+		},
+
+		isLastDayOfTheMonth: function (date) {
+			var test = new Date(date.getTime());
+			test.setDate(test.getDate() + 1);
+			return test.getDate() === 1;
+		},
+
+		isInSameMonth: function (date1, date2) {
+			return (date1.getFullYear() === date2.getFullYear()) && (date1.getMonth() === date2.getMonth());
 		}
+
 	};
 }]);
