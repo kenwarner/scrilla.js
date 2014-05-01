ba6f3f9649704f83e01ba498b9fe00c40de35995
Ken Warner <kjwarner@gmail.com>
2014-05-01 00:53:30 -0400

    update a few nuget packages

--- scrilla.js.Web/Global.asax.cs
+++ scrilla.js.Web/Global.asax.cs
@@ -1,18 +1,10 @@
-﻿using Castle.Windsor;
-using Castle.Windsor.Installer;
-using scrilla.Data.Dapper;
-using scrilla.js.Web.App_Start;
-using scrilla.js.Web.Composition;
-using scrilla.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.Dispatcher;
-using System.Web.Mvc;
 using System.Web.Optimization;
+using scrilla.js.Web.Composition;
+using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
 
 namespace scrilla.js.Web
 {
