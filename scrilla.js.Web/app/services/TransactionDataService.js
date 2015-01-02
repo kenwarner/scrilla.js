540c205c4c8cd6b61cd363be2f248e0e12bc58e0
Ken Warner <kjwarner@gmail.com>
2015-01-02 00:07:31 -0500

    can just send $stateParams to api endpoints. beautiful!

--- scrilla.js.Web/app/services/TransactionDataService.js
+++ scrilla.js.Web/app/services/TransactionDataService.js
@@ -1,8 +1,8 @@
-angular.module('scrilla').factory('TransactionDataService', ['$http', function ($http) {
+angular.module('scrilla').factory('TransactionDataService', ['$http', '$stateParams', function ($http, $stateParams) {
 	var service = {};
 
 	service.getTransactions = function () {
-		return $http.get('/api/transactions', { params: { from: '2012-01-01', to: '2012-02-01' } });
+		return $http.get('/api/transactions', { params: $stateParams });
 	};
 
 	return service;
