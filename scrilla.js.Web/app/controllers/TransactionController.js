956465a3ed3b3cab51685b7968afa4d2e6789a5a
Ken Warner <kjwarner@gmail.com>
2015-01-03 13:09:21 -0500

    get the gripApi object

--- scrilla.js.Web/app/controllers/TransactionController.js
+++ scrilla.js.Web/app/controllers/TransactionController.js
@@ -11,6 +11,9 @@ angular.module('scrilla').controller('TransactionController', ['TransactionDataS
 		enableRowSelection: true,
 		selectionRowHeaderWidth: 35,
 
+		//minRowsToShow: data.length,
+		//data: data,
+
 		//infiniteScroll: 5,//50 / data.length * 100,
 		columnDefs: [
 			{ name: 'Date', field: 'timestamp', cellFilter: 'date:"yyyy-MM-dd"', maxWidth: 1, enableFiltering: false },
@@ -18,12 +21,20 @@ angular.module('scrilla').controller('TransactionController', ['TransactionDataS
 			{ name: 'Vendor', field: 'vendorName' },
 			{ name: 'Category', field: 'categoryName' },
 			{ field: 'amount', cellFilter: 'parenlessCurrency', maxWidth: 1, enableFiltering: false, enableCellEdit: false }
-		]
+		],
+
+		onRegisterApi: function(gridApi) {
+			vm.gridApi = gridApi;
+		}
 	};
 
-	var setGridData = function(data) {
+	var setGridData = function (data) {
 		vm.gridOptions.minRowsToShow = data.length;
 		vm.gridOptions.data = data;
+
+		if (angular.isDefined(vm.gridApi)) {
+			vm.gridApi.core.refresh();
+		}
 	};
 
 	vm.getTransactions = function () {
@@ -35,6 +46,7 @@ angular.module('scrilla').controller('TransactionController', ['TransactionDataS
 	vm.getAllTransactions = function() {
 		TransactionDataService.getAllTransactions().success(function (data) {
 			allTransactions = data;
+			setGridData(data);
 		});
 	};
 
