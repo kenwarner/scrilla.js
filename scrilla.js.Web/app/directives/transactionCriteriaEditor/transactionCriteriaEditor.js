5878aa8d0005bc6c17ed61dd5d1caba0f9f46809
Ken Warner <kjwarner@gmail.com>
2015-01-03 22:54:16 -0500

    add transaction-criteria-editor to transactions grid

--- scrilla.js.Web/app/directives/transactionCriteriaEditor/transactionCriteriaEditor.js
+++ scrilla.js.Web/app/directives/transactionCriteriaEditor/transactionCriteriaEditor.js
@@ -4,7 +4,7 @@ angular.module('scrilla').directive('transactionCriteriaEditor', ['$state', '$st
 		replace: true,
 		templateUrl: 'app/directives/transactionCriteriaEditor/transactionCriteriaEditor.html',
 		link: function (scope) {
-			scope.$watchGroup(['$stateParams.from', '$stateParams.to'], function (newValue, oldValue) {
+			scope.$watchGroup(['$stateParams.from', '$stateParams.to', '$stateParams.vendorId'], function (newValue, oldValue) {
 				if (newValue !== oldValue && !$state.current.abstract) {
 					$state.transitionTo($state.current, $stateParams, { notify: false });
 				}
